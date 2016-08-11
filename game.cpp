fc8ea8dc0e6ca73a70e0ad22ef5c978de2263d77
Gustvo <gustvoasbezerra@gmail.com>
2016-08-11 18:50:25 -0300

    eddited source to conform the change in the file move

--- game.cpp
+++ game.cpp
@@ -4,37 +4,37 @@ JANELAS game(sf::RenderWindow* window){
 
     sf::Texture t103, t152, t68, t44, t40, t57, t45, t154, t32, t20;
 
-    if(!t152.loadFromFile("Tiles\\grassCenter.png")){
+    if(!t152.loadFromFile("src\\Tiles\\grassCenter.png")){
         std::cout << "ERROR";
     }
-    if(!t32.loadFromFile("Tiles\\grassHillLeft.png")){
+    if(!t32.loadFromFile("src\\Tiles\\grassHillLeft.png")){
         std::cout << "ERROR";
     }
-    if(!t20.loadFromFile("Tiles\\grassHillLeft2.png")){
+    if(!t20.loadFromFile("src\\Tiles\\grassHillLeft2.png")){
         std::cout << "ERROR";
     }
-    if(!t154.loadFromFile("Tiles\\stoneCenter.png")){
+    if(!t154.loadFromFile("src\\Tiles\\stoneCenter.png")){
         std::cout << "ERROR";
     }
-    if(!t40.loadFromFile("Tiles\\signRight.png")){
+    if(!t40.loadFromFile("src\\Tiles\\signRight.png")){
         std::cout << "ERROR";
     }
-    if(!t57.loadFromFile("Tiles\\door_OpenMid.png")){
+    if(!t57.loadFromFile("src\\Tiles\\door_OpenMid.png")){
         std::cout << "ERROR";
     }
-    if(!t45.loadFromFile("Tiles\\door_OpenTop.png")){
+    if(!t45.loadFromFile("src\\Tiles\\door_OpenTop.png")){
         std::cout << "ERROR";
     }
-    if(!t68.loadFromFile("Tiles\\grassHalfLeft.png")){
+    if(!t68.loadFromFile("src\\Tiles\\grassHalfLeft.png")){
         std::cout << "ERROR";
     }
-    if(!t44.loadFromFile("Tiles\\grassHalfRight.png")){
+    if(!t44.loadFromFile("src\\Tiles\\grassHalfRight.png")){
         std::cout << "ERROR";
     }
-    if(!t103.loadFromFile("Tiles\\grassMid.png")){
+    if(!t103.loadFromFile("src\\Tiles\\grassMid.png")){
         std::cout << "ERROR";
     }
-    if(!tplayer.loadFromFile("NPC2.png")){
+    if(!tplayer.loadFromFile("src\\NPC2.png")){
         std::cout << "ERROR";
     }
 
